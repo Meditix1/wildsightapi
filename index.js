72e9f547025a371b76fdebf4912f27e4f7bbcfab
Sakshi Singh <66342708+SakshiSinghh@users.noreply.github.com>
2024-11-05 07:36:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const BASE_URL = process.env.BASE_URL || `https://default-blackbird-rporaxgmail-coms-organization-e6504-0.blackbird-relay.a8r.io/wildlsightapi`;
+
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/', routes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on ${BASE_URL}/${PORT}`);
 });
