8d238e618dffa37be150dbefec4a81037072709e
Sakshi Singh <66342708+SakshiSinghh@users.noreply.github.com>
2024-11-05 09:05:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,20 +5,11 @@ const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 80;
-const BASE_URL = process.env.BASE_URL || `https://default-blackbird-rporaxgmail-coms-organization-e6504-0.blackbird-relay.a8r.io/wildlsightapi`;
-
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/', routes);
 
-
-// Error handling middleware should be added here
-app.use((err, req, res, next) => {
-    console.error(err.stack); // Log the error stack
-    res.status(500).send('Something went wrong!'); // Respond with a 500 status
-});
-
 app.listen(PORT, () => {
-    console.log(`Server is running on ${BASE_URL}/${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
